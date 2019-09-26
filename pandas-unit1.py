f1f553a41126cd5531082d23a701026e4689daad
ajay-data-experts <55768898+ajay-data-experts@users.noreply.github.com>
2019-09-25 23:39:00 -0700

    Update pandas-unit1.py

--- pandas-unit1.py
+++ pandas-unit1.py
@@ -6,3 +6,16 @@
 
 import numpy as np
 import pandas as pd
+
+#series
+#panda series contains both sequence of indices and sequence of values
+data = pd.Series([0.25, 0.5, 0.75, 1.0])
+print(data)
+
+#values
+data.values
+
+#index
+data.index
+#however use it like 
+data[1]
